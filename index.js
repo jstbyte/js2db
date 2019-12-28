109e3fa54a0f8d32e9a77b10ad6757bf885ea4d3
Jahid <A29.Connect@Gmail.com>
2019-12-28 12:07:31 +0530

    Complate Api Added. Ready To Use.

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-module.exports = require('./lib/js2db');
+//module.exports = require('./lib/js2db');
+const Js2db = require('./lib/js2db')
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  
+
+
+
+const db = new Js2db();
+
+db.open('mydb').then(()=>{
+
+    console.log('Loded');
+
+    readline.question(`What's your name?  :`, (name) => {
+
+        db.insert('mydb', {
+            name, age: 20, addrs: "Ambari"
+        }).then((val) => {
+
+            console.log('\n--inserted--\n');
+            console.log(val);
+
+        }).catch(console.log);
+
+
+
+        db.find('mydb', {}).then(val => {
+
+            console.log('\n--Query Result--\n');
+            console.log(val);
+
+        });
+
+
+        db.update('mydb', {name}, {name:`Mr. ${ name }`}).then(val=>{
+
+            console.log('\n--Updated--\n');
+            console.log(val);
+
+        });
+
+
+
+        db.update('mydb', {name:`Mr. ${ name }`}, {name:`Md. ${ name } `}).then(val=>{
+
+            console.log('\n--Updated--\n');
+            console.log(val);
+
+        });
+
+
+        db.find('mydb', {}).then(val => {
+
+            console.log('\n--Query Result--\n');
+            console.log(val);
+
+        });
+
+        db.remove('mydb', {name}).then(val=>{
+
+            console.log('\n--Removed--\n');
+            console.log(val);
+
+        });
+
+
+
+        readline.close()
+      });
+
+
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+## See DataBase Docs At <strong> nedb <strong> .
+### I Just Make It Promise Based With Cache System .
+### I Build This System To Use In Express .
+### To Diretcly Use DataBase From Angular Or React Or Plain Fatch Api.
